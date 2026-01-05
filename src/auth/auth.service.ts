ab15065dc7cd8604663716a1048f3d9172ed8309
Muhammadali Jamolov <boburjonr934@gmail.com>
2026-01-05 13:21:48 +0500

    working on jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,19 +3,23 @@ import { signUpDto } from './dto/sign-up.dto';
 import { db } from 'src/db/drizzle';
 import { usersTable } from 'src/db/schema';
 import { eq } from 'drizzle-orm';
+import { JwtService } from '@nestjs/jwt';
 
-const JWT_SECRET = process.env.JWT_SECRET;
 const SHORT_EXPIRE_DATE = "5m";
 const LONG_EXPIRE_DATE = "23d";
 
 @Injectable()
 export class AuthService {
+    constructor(
+        private jwtService: JwtService
+    ){}
+
     async signUp(signUpDto: signUpDto) {
         const existingUser = await db.select().from(usersTable).where(eq(usersTable.email, signUpDto.email));
         if (existingUser.length > 0) {
             throw new ConflictException("User already exists.");
         };
 
-        
+        const token = 'token';
     };
 };
